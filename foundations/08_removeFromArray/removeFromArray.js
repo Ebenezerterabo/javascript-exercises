8fbc18f3c15ba00d12ee322d7517874b8d3be1bb
Ebenezerterabo <teraboebenezer@gmail.com>
2026-05-27 22:32:20 +0000

    Remove from an array function created

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, removeItem) {
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === removeItem) {
+            arr.splice(i, 1);
+            break;
+        }
+    }
+    return arr;
+
 };
+
+console.log(removeFromArray([1,2,3,4], 1))
 
 // Do not edit below this line
 module.exports = removeFromArray;
