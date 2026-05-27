b147eace506cddb749cb9aa6fedb4d59c167dfa8
Ebenezerterabo <teraboebenezer@gmail.com>
2026-05-27 21:07:50 +0000

    wrote a function that repeats a string a number of times

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    
+    const newStr = [];
+    
+    if (num < 0) return "ERROR";
+    
+    for (let i = 0; i < num; i++) {
+        newStr.push(str);
+    }
+    
+    return newStr.join("");
 };
+
+console.log(repeatString("hello", 4));
 
 // Do not edit below this line
 module.exports = repeatString;
