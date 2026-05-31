0774aeaac3c2b2b3666e697611be4e9e8c54e0c8
Ebenezerterabo <teraboebenezer@gmail.com>
2026-05-31 21:00:53 +0000

    Completed the Celsius and fahrenheit converter function

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const fraction = 5 / 9;
+
+  const result = (fahrenheit - 32) * fraction;
+
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const revFraction = 9 / 5;
+
+  const result = (celsius * revFraction) + 32;
+
+  return Math.round(result * 10) / 10;
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(100));
 
 // Do not edit below this line
 module.exports = {
