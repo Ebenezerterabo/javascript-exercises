54d826520a564524fa180c0f71315ab710b37cf8
Ebenezerterabo <teraboebenezer@gmail.com>
2026-05-31 18:35:15 +0000

    Sum all numbers with the range of value passed

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    let sum = 0;
 
+    for (let i = start; i <= finish; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+console.log(sumAll(2, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
