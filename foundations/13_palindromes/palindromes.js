793a228311a74f54321219f4fbe38c939a21b2f2
Ebenezerterabo <teraboebenezer@gmail.com>
2026-08-15 23:13:16 +0000

    Palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    // check string for valid characters
+    newStr = str
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphanumerical.includes(char))
+        .join("");
+    reversedStr = newStr.split("").reverse().join("");
+
+    if (newStr === reversedStr) {   
+        return true;
+    } else {
+        return false;
+    }
 };
+
+
+
+console.log(palindromes("r3ace3car"));
+// console.log(add(1, 2));
 
 // Do not edit below this line
 module.exports = palindromes;
