5cc385e5815e3c5b257d8cffa654261b052e11d1
Ebenezerterabo <teraboebenezer@gmail.com>
2026-08-16 08:05:38 +0000

    getTheTitles solved

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const titles = array.map((book) => book.title);
+    return titles
 };
 
+// const books = [
+//     {
+//         title: 'Book',
+//         author: 'Name'
+//     },
+//     {
+//         title: 'Book2',
+//         author: 'Name2'
+//     }
+// ]
+
+
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
