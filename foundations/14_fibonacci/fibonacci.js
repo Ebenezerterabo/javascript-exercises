896d4ae84e2c8895849dafdd6bbcb00006d177b4
Ebenezerterabo <teraboebenezer@gmail.com>
2026-08-16 07:46:29 +0000

    fibonacci solved

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
 
+    if (position < 0) return "OOPS";
+    if (position == 0) return 0;
+
+    let array = [1, 1];
+    for (let i = 2; i <= position; i++) {
+        // array[i] = array[i - 1] + array[i - 2];
+        array.push(array[i - 1] + array[i - 2]);        
+    }
+    return array[position - 1];
 };
 
+console.log(fibonacci(-10));
 // Do not edit below this line
 module.exports = fibonacci;
